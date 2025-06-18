463e25fafd3109e6e4f2176d3571c4cc826decd6
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-06-18 12:29:17 -0300

    Criado a camada de apresentação.

---
+++ GerFuncProd.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GerFuncProd.Mvc.MvcApplication" Language="C#" %>
---
+++ GerFuncProd.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GerFuncProd.Mvc.Startup))]
+namespace GerFuncProd.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
