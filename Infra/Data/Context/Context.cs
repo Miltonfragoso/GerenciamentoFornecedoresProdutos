6b9a3c629554da36881a50232be7d32d9c6777ae
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-06-27 03:10:27 -0300

    Iniciando a configuração do DbContext

---
+++ Infra/Data/Context/Context.cs
@@ -0,0 +1,20 @@
+﻿using GerFuncProd.Business.Models.Fornecedores;
+using GerFuncProd.Business.Models.Produtos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Context
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection")
+        { }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedors { get; set; }
+    }
+}
