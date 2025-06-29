c9e943ded53430b3be082f30eca0ef2439f4cc38
Milton Fragoso <miltonfragoso13@yahoo.com.br>
2025-06-29 10:36:13 -0300

    Nesta aula feito o mapeamento das entidades com o FluentAPI, sobrescrista do método OnModelCreating e feito a abilitação da migration.

--- Infra/Data/Context/Context.cs
+++ Infra/Data/Context/Context.cs
@@ -1,9 +1,12 @@
 ﻿using GerFuncProd.Business.Models.Fornecedores;
 using GerFuncProd.Business.Models.Produtos;
+using Infra.Data.Mappings;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +19,21 @@ namespace Infra.Data.Context
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
         public DbSet<Fornecedor> Fornecedors { get; set; }
+
+
+        //É um método virtual fornecido pela classe DbContext, que permite configurar o mapeamento entre suas classes (entidades)
+        //e o banco de dados de forma mais avançada Configuração via Fluent API
+       //Permite definir regras de mapeamento como Nome da tabela ou coluna Tamanho de campos(HasMaxLength) Chaves primárias e estrangeiras
+       //Relacionamentos(HasOne, HasMany, etc.)  Restrições e índices
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Configurations.Add(new FornecedorConfig());
+            modelBuilder.Configurations.Add(new EnderecoConfig());
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+
+        }
+
+
     }
 }
---
+++ Infra/Data/Migrations/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Infra.Data.Context;
+using System.Data.Entity.Migrations;
+
+namespace Infra.Migrations
+{
+
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    }
+}
